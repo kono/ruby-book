7100fec4d087f33caf54aafa18163027228078b5
Hiroshi Kono <hiroshi.kono@gmail.com>
2017-11-20 16:10:50 +0900

    Add comment(example) about scan function (p.112)

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -5,8 +5,12 @@ def to_hex(r, g, b)
 end
 
 def to_ints(hex)
-  r, g, b = hex[1..2], hex[3..4], hex[5..6]
+  r, g, b = hex.scan(/\w\w/)
   [r, g, b].map do |s|
     s.hex
   end
 end
+# '#12abcd'.scan(/\w/)       #=> ["1", "2", "a", "b", "c", "d"]
+# '#12abcd'.scan(/\w\w/)     #=> ["12", "ab", "cd"]
+# '#12abcd'.scan(/\w\w\w/)   #=> ["12a", "bcd"]
+# '#12abcd'.scan(/\w\w\w\w/) #=> ["12ab"]
