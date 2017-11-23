01557956059bb69d916da8e498cc434a228ccd2e
Hiroshi Kono <Hiroshi.Kono@gmail.com>
2017-11-23 14:37:10 +0900

    Implement Gate class(p.229)

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -3,11 +3,14 @@ require './lib/gate'
 require './lib/ticket'
 
 class GateTest < Minitest::Test
-  def test_gate
+
+  def test_umeda_to_mikuni_when_fare_is_not_enough
     umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
+    mikuni = Gate.new(:mikuni)
+
     ticket = Ticket.new(150)
     umeda.enter(ticket)
-    assert juso.exit(ticket)
+    refute mikuni.exit(ticket)
+
   end
 end
