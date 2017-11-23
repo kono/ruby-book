6425f35e8814dcca80a208fed31f432178c4be5c
Hiroshi Kono <Hiroshi.Kono@gmail.com>
2017-11-23 14:12:46 +0900

    "Fake it" implement Gate and Ticket(p.226)

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -1,9 +1,13 @@
 require 'minitest/autorun'
 require './lib/gate'
+require './lib/ticket'
 
 class GateTest < Minitest::Test
   def test_gate
-    # とりあえずGateObjectが作れることを確認する
-    assert Gate.new
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    assert juso.exit(ticket)
   end
 end
