e25eec1e7e0399555d1a57be0f0ae9d8f36cdb08
Hiroshi Kono <Hiroshi.Kono@gmail.com>
2017-11-23 14:42:52 +0900

    Refactoring tests(p.231)

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -4,22 +4,20 @@ require './lib/ticket'
 
 class GateTest < Minitest::Test
 
-  def test_umeda_to_juso
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
-
-    ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    assert juso.exit(ticket)
+  def setup
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
   end
-  
-  def test_umeda_to_mikuni_when_fare_is_not_enough
-    umeda = Gate.new(:umeda)
-    mikuni = Gate.new(:mikuni)
-
+  def test_umeda_to_juso
     ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    refute mikuni.exit(ticket)
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket)
+  end
 
+  def test_umeda_to_mikuni_when_fare_is_not_enough
+    ticket = Ticket.new(150)
+    @umeda.enter(ticket)
+    refute @mikuni.exit(ticket)
   end
 end
