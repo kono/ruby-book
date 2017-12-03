b163970531227283a2617588e6737b5dae29a4a9
Hiroshi Kono <Hiroshi.Kono@gmail.com>
2017-12-03 17:10:25 +0900

    Make deep_freeze function available for hash data (p.294)

--- test/deep_freezable_test.rb
+++ test/deep_freezable_test.rb
@@ -12,4 +12,15 @@ class DeepFreezableTest < Minitest::Test
     # 配列の要素がすべてfreezeされているか?
     assert Team::COUNTRIES.all?{|country| country.frozen?}
   end
+  def test_deep_freeze_to_hash
+    # ハッシュの値は正しいか?
+    assert_equal(
+    {'Japan' => 'yen', 'US' => 'doller', 'India' => 'rupee'},
+    Bank::CURRENCIES
+    )
+    # ハッシュ自身がfreezeされているか?
+    assert Bank::CURRENCIES.frozen?
+    # ハッシュの要素(キーと値)がすべてfreezeされているか?
+    assert Bank::CURRENCIES.all? {|key, value| key.frozen? && value.frozen? }
+  end
 end
